ba144bbcb2b03891a30f5a2e4c2942057b9cbe19
Pete Shadbolt <pete.shadbolt@gmail.com>
2013-08-01 09:32:47 +0100

    awd

--- main.js
+++ main.js
@@ -13,17 +13,29 @@ function parseRSS(url, callback)
   });
 }
 
+function stripTitle(oldtitle)
+{
+    return oldtitle.replace(/ *\([^)]*\) */g, "");
+}
+
+
+// this gets called when the rss is loaded
+function populate(rssjson)
+{
+    
+    $('p').text('awd');
+    for (var i=0; i<rssjson.entries.length; i++)
+    { 
+        var entry=rssjson.entries[i]
+        console.log(entry.title);
+        $('#container').append(entry.title);
+        //document.write(cars[i] + "<br>");
+    }
+}
+
+
 // the main function
 function main() 
 {
     parseRSS('http://arxiv.org/rss/quant-ph', populate);
 }
-
-// this gets called when the rss is loaded
-function populate(newtext)
-{
-    $('p').text('awd');
-    console.log(newtext);
-}
-
-
