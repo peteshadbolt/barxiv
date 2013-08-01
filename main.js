8832be225830f1d8a5c84aae4d9d90be14d55753
Pete Shadbolt <pete.shadbolt@gmail.com>
2013-08-01 12:06:20 +0100

    todo: authors

--- main.js
+++ main.js
@@ -13,28 +13,58 @@ function parseRSS(url, callback)
   });
 }
 
-function stripTitle(oldtitle)
-{
-    return oldtitle.replace(/ *\([^)]*\) */g, "");
+// get the query string
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+// clean up the title
+function stripTitle(oldtitle) { return oldtitle.replace(/ *\([^)]*\) */g, ""); }
+
+// clean up the authors
+function stripAuthors(full) {
+    console.log(full);
+    //full=full.replaceAll("\&lt", "\<");
+    //full=full.replaceAll("\&gt", "\>");
+    return full.replaceAll("<[^>]*>", "");
+}
+
+// format a post
+function getPost(entry)
+{
+    var post='';
+    post+='<div class="post">';
+    post+='<div>';
+    post+='<span class="title">';
+    post+=stripTitle(entry.title);
+    post+='</span>';
+    post+='</div>';
+
+    post+='<div class="authors">';
+    //post+=stripAuthors(entry.author);
+    post+='awd'
+    post+='</div>';
+    post+='</div>';
+    return post;
+}
 
 // this gets called when the rss is loaded
 function populate(rssjson)
 {
     
-    $('p').text('awd');
+    $('p').text('Loading...');
     for (var i=0; i<rssjson.entries.length; i++)
     { 
-        var entry=rssjson.entries[i]
-        console.log(entry.title);
-        $('#container').append(entry.title);
-        //document.write(cars[i] + "<br>");
+        var entry=rssjson.entries[i];
+        post=getPost(entry);
+        $('#container').append(post);
     }
 }
 
-
-// the main function
+// the main function, gets called when the page loads
 function main() 
 {
     parseRSS('http://arxiv.org/rss/quant-ph', populate);
