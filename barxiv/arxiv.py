d017ff77890b6ea766d8aea529ea0cddee8887e0
Pete Shadbolt <pete.shadbolt@gmail.com>
2013-11-28 00:21:17 +0000

    started working on database

--- barxiv/arxiv.py
+++ barxiv/arxiv.py
@@ -4,77 +4,77 @@ from time import mktime
 import feedparser
 from optimize import optimize_search
 
-max_results=100
-
 def strip_authors(html): return re.sub('<[^>]*>', '', html)
 def strip_tags(html): return re.sub('<[^>]*>', '', html)
 def strip_title(title): return title.split('(')[0]
 def epoch(dt): return int(mktime(dt.timetuple()))
 
-def parse_api((index, entry)):
-    ''' parse a post from the arxiv '''
-    out={}
-    out['title']=entry['title'].replace('\n', '')
-    out['id']=entry['id']
-    out['abstract']=strip_tags(entry['summary'])
-    out['authors']=', '.join(map(lambda x: x['name'], entry['authors']))
-    out['search']=optimize_search(out['title']+out['abstract']+out['authors'])
-    out['link']=entry['link'].replace('abs', 'pdf')
-    t1=datetime.strptime(entry['published'].split('T')[0], '%Y-%m-%d')
-    t2=datetime.strptime(entry['updated'].split('T')[0], '%Y-%m-%d')
-    out['published']='%s' % t2.strftime('%A %d %B')
-    out['index']=index
-    out['epoch']=epoch(t2)
-    if not t2==t1: return None
-    return out
+class post:
+    def __init__(self, entry):
+        ''' A post on the arxiv '''
+        self.arxiv_id=entry['id'].split('/')[-1]
+        self.title=strip_title(entry['title'].replace('\n', ''))
+        self.abstract=strip_tags(entry['summary'])
+        self.authors=strip_authors(entry['author'])
+        self.search_terms=optimize_search(self.title+self.abstract+self.authors)
+        self.pdf_url=entry['link'].replace('abs', 'pdf')
+        t1=datetime.today()
+        self.published_epoch=epoch(t1)
 
-def parse_rss((index, entry)):
-    ''' parse a post from the arxiv '''
-    out={}
-    out['title']='<img src="today.png"/>'
-    out['id']=entry['id']
-    out['title']+=strip_title(entry['title'].replace('\n', ''))
-    if 'UPDATED' in entry['title']: return None
-    out['abstract']=strip_tags(entry['summary'])
-    out['authors']=strip_authors(entry['author'])
-    out['search']=optimize_search(out['title']+out['abstract']+out['authors'])
-    out['link']=entry['link'].replace('abs', 'pdf')
-    t1=datetime.today()
-    out['published']='New'
-    out['index']=index
-    out['epoch']=epoch(t1)
-    return out
-
-def get_api():
-    ''' get the latest n posts from the arxiv as a list of dicts '''
-    api='http://export.arxiv.org/api/query?search_query=cat:quant-ph&start=0&max_results=%d&sortBy=submittedDate&sortOrder=descending' % max_results
-    feed = feedparser.parse(api)
-    return list(map(parse_api, enumerate(feed.entries)))
+    def __str__(self):
+        ''' Convert to string '''
+        return '%s: %s' % (self.arxiv_id, self.title)
 
 def get_rss():
     ''' get the latest n posts from the arxiv as a list of dicts '''
-    rss='http://arxiv.org/rss/quant-ph'
-    feed = feedparser.parse(rss)
-    return list(map(parse_rss, enumerate(feed.entries)))
+    rss_url='http://arxiv.org/rss/quant-ph'
+    feed = feedparser.parse(rss_url)
+    posts = list(map(post, feed.entries))
+    return posts
 
-def get_all(category=None):
-    ''' get all posts from the arxiv '''
-    api_posts=get_api()
-    rss_posts=list(reversed(get_rss()))
-    posts=rss_posts+api_posts
-    posts=filter(lambda x: x!=None, posts)
+#def get_api():
+    #''' get the latest n posts from the arxiv as a list of dicts '''
+    #api='http://export.arxiv.org/api/query?search_query=cat:quant-ph&start=0&max_results=%d&sortBy=submittedDate&sortOrder=descending' % max_results
+    #feed = feedparser.parse(api)
+    #return list(map(parse_api, enumerate(feed.entries)))
 
-    # remove dupicates
-    unique_posts=[]
-    used_ids=[]
-    for post in posts:
-        ft=strip_tags(post['title'].lower()).replace(' ', '').replace('.', '').strip()
-        if not ft in used_ids:
-            unique_posts.append(post)
-            used_ids.append(ft)
+#def parse_api((index, entry)):
+    #''' parse a post from the arxiv '''
+    #out={}
+    #out['title']=entry['title'].replace('\n', '')
+    #out['id']=entry['id']
+    #out['abstract']=strip_tags(entry['summary'])
+    #out['authors']=', '.join(map(lambda x: x['name'], entry['authors']))
+    #out['search']=optimize_search(out['title']+out['abstract']+out['authors'])
+    #out['link']=entry['link'].replace('abs', 'pdf')
+    #t1=datetime.strptime(entry['published'].split('T')[0], '%Y-%m-%d')
+    #t2=datetime.strptime(entry['updated'].split('T')[0], '%Y-%m-%d')
+    #out['published']='%s' % t2.strftime('%A %d %B')
+    #out['index']=index
+    #out['epoch']=epoch(t2)
+    #if not t2==t1: return None
+    #return out
 
-    return unique_posts
 
-if __name__=='__main__':
-    posts=get_all()
+#def get_all(category=None):
+    #''' get all posts from the arxiv '''
+    #api_posts=get_api()
+    #rss_posts=list(reversed(get_rss()))
+    #posts=rss_posts+api_posts
+    #posts=filter(lambda x: x!=None, posts)
+
+     #remove dupicates
+    #unique_posts=[]
+    #used_ids=[]
+    #for post in posts:
+        #ft=strip_tags(post['title'].lower()).replace(' ', '').replace('.', '').strip()
+        #if not ft in used_ids:
+            #unique_posts.append(post)
+            #used_ids.append(ft)
+
+    #unique_posts=filter(lambda x: x!=None, unique_posts)
+    #return unique_posts
+
+#if __name__=='__main__':
+    #posts=get_all()
 
--- barxiv/barxiv.py
+++ barxiv/barxiv.py
@@ -1,74 +1,48 @@
-import re
 import webapp2
-import json, time, logging, os
+from google.appengine.ext import ndb
 import jinja2
-import codecs
+import json, time, logging, os, re
 import arxiv
-import nature
-import science
+
+DEFAULT_DATABASE_NAME='BARXIV'
+def post_database_key(): return ndb.Key('Post', DEFAULT_DATABASE_NAME)
+
+class post(ndb.Model):
+    ''' Model a post in the database '''
+    token=ndb.StringProperty()
+    name=ndb.StringProperty()
+    email=ndb.StringProperty()
+    intent=ndb.StringProperty()
+    state=ndb.StringProperty()
+    duration=ndb.IntegerProperty()
+    date=ndb.DateTimeProperty(auto_now_add=True)
+    started=ndb.DateTimeProperty()
 
 # Set up the templating engine
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'])
 
-cutoff=100
-
-def trim_authors(post, chop=300):
-    ''' Get rid of this '''
-    if len(post['authors'])>chop:
-        post['authors']=post['authors'][:chop]+'...'
-    return post
-
-def scrape_site(source, name):
-    ''' load posts, filter them, sort them, and dump to JSON '''
-    # this should write to a datastore
-    # we should use hashes to identify unique posts to the arxiv
-    all_posts=source.get_all(name)
-    all_posts=filter(lambda x: x!=None, all_posts)
-    all_posts=sorted(all_posts, key=lambda x: x['epoch'], reverse=True)
-    all_posts=map(trim_authors, all_posts)
-    s=json.dumps(all_posts[:cutoff], indent=2)
-    logging.info(s)
-    #f=open('cache/%s.json' % name, 'w')
-    #f.write(s)
-    #f.close()
-
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        ''' Build the page and send it over '''
-        # Construct a string detailing the access token state
-        #access_token_state=database.check_token(self.request.get('accessToken').strip())
-
-        # Template the page and send it back to the user
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        #template_values={'access_token_message': access_token_state['message']}
-        template_values={}
-        self.response.out.write(template.render(template_values))
-
-class GetChipPage(webapp2.RequestHandler):
-    def get(self):
-        ''' User asked for a chip '''
-        chip = self.request.get('chipName')
-        chip_json = chip_library.chip_data[chip]
-        self.response.headers['Content-Type'] = "application/json"
-        self.response.out.write(json.dumps(chip_json))
-
 class ScrapePage(webapp2.RequestHandler):
     def get(self):
         ''' Time to scrape the journals '''
         logging.info('Starting scrape')
-        scrape_site(arxiv, 'quant-ph')
-        scrape_site(science, 'science')
-        scrape_site(nature, 'nature')
-        scrape_site(nature, 'nphoton')
-        scrape_site(nature, 'nphys')
-        scrape_site(nature, 'ncomms')
-        logging.info('Scrape done')
+        all_posts=arxiv.get_rss()
+        for post in all_posts:
+            continue
+            token = accessToken(parent=access_token_database_key())
+            # build the token here
+            token.put()
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        ''' Build the page and send it over '''
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        template_values={}
+        self.response.out.write(template.render(template_values))
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/chip', GetChipPage),
-    ('/admin/scrape', ScrapePage),
-], debug=False)
+    ('/admin/scrape', ScrapePage)], debug=False)
+
 
