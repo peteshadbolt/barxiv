eee905cb4c885922b4a0a4228379bd8f5bacc483
Pete Shadbolt <pete.shadbolt@gmail.com>
2013-12-03 18:11:58 +0000

    small things

--- barxiv/barxiv.py
+++ barxiv/barxiv.py
@@ -9,7 +9,7 @@ def post_database_key(): return ndb.Key('Post', DEFAULT_DATABASE_NAME)
 
 class Post(ndb.Model):
     ''' Model a post in the database '''
-    arxiv_id=ndb.TextProperty()
+    arxiv_id=ndb.StringProperty()
     title=ndb.TextProperty()
     abstract=ndb.TextProperty()
     authors=ndb.TextProperty()
@@ -27,16 +27,16 @@ class MainPage(webapp2.RequestHandler):
         ''' Build the page and send it over '''
         all_posts='posts'
 
-        query = Post.query(ancestor=post_database_key()).order(-Post.scraped)
+        query = Post.query(ancestor=post_database_key()).order(-Post.published)
         latest_posts = query.fetch(None)
 
         all_posts=''
         template = JINJA_ENVIRONMENT.get_template('post.html')
         for post in latest_posts:
-            template_values={'url':post.arxiv_id, 
-                             'title':post.title, 
-                             'authors':post.authors, 
-                             'new': 'new?',
+            template_values={'url': 'http://arxiv.org/pdf/%s' % post.arxiv_id, 
+                             'title': post.title, 
+                             'authors': post.authors, 
+                             'new': post.published.strftime('%A %d %B'),
                              'abstract': 'no abstract'}
             all_posts+=template.render(template_values)
 
@@ -52,27 +52,31 @@ class ScrapePage(webapp2.RequestHandler):
     def get(self):
         ''' A cron job said that it's time to scrape the journals '''
         logging.info('Starting scrape')
-
-        # Clear out the database
-        ndb.delete_multi(Post.query().fetch(keys_only=True))
+        summary=''
+        number_new=0
 
         # Put the new posts into the database
-        #TODO: check for duplicates here
         all_posts=arxiv.get_rss()
         for post in all_posts:
-            db_entry = Post(parent=post_database_key(),  \
-                        id=post.arxiv_id,  \
-                        arxiv_id=post.arxiv_id, \
-                        title=post.title,  \
-                        abstract=post.abstract,  \
-                        authors=post.authors,  \
-                        search_terms=post.search_terms,  \
-                        published=post.published)
-            db_entry.put()
-        logging.info('Scrape finished')
+            # See if the post is already in the database
+            if Post.query(Post.arxiv_id==post.arxiv_id).get()!=None:
+                summary+='%s is already in the database<br>' % post.arxiv_id
+            else:
+                summary+='%s is new, added it to the database<br>' % post.arxiv_id
+                db_entry = Post(parent=post_database_key(),  \
+                            id=post.arxiv_id,  \
+                            arxiv_id=post.arxiv_id, \
+                            title=post.title,  \
+                            abstract=post.abstract,  \
+                            authors=post.authors,  \
+                            search_terms=post.search_terms,  \
+                            published=post.published)
+                number_new+=1
+                db_entry.put()
 
         # Build a little summary page
-        summary='Logged %d articles from the arXiv of which (how many?) were duplicates' % len(all_posts)
+        summary=('Scraped %d articles. %d new articles added<br>' % (len(all_posts), number_new))+summary
+        logging.info('Scraped %d articles. %d new articles added' % (len(all_posts), number_new))
         self.response.out.write(summary)
 
 application = webapp2.WSGIApplication([
